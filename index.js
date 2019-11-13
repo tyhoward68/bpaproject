4c76948d24a9b8f3799020c6065cd0125876cdac
unknown <PS20VAdhikari@efcts.us>
2019-11-13 14:09:56 -0500

    cartview

--- index.js
+++ index.js
@@ -14,4 +14,12 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`)) 
 
+  router.get('/shopping-cart', function(req, res, next) {
+    if(!req.session.cart){
+      return res.render('shop/shopping-cart', {products:null});
+    }
+    var cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart',{products: cart.generateArray(), totalPrice: cart.totalPrice})
+  });
+
 
