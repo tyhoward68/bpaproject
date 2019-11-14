044c1e66e1426f021b3909c931aa08b72049a715
unknown <PS20VAdhikari@efcts.us>
2019-11-14 12:24:27 -0500

    cart

--- models/cart.js
+++ models/cart.js
@@ -1,8 +1,9 @@
+//
 module.exports = function Cart(oldCart) {
   this.items = oldCart.items || {};
-  this.totalQty = oldCart.totalQty || 0 ;
-  this.totalPrice = oldCart.totalPrice || 0;
-
+  this.totalItemQuantity = oldCart.totalItemQuantity || 0 ;
+  this.totalItemPrice = oldCart.totalItemPrice || 0;
+//pricing of items
   this.add = function(item, id) {
     var storedItem = this.items[id];
     if (!storedItem) {
@@ -10,10 +11,10 @@ module.exports = function Cart(oldCart) {
     }
     storedItem.qty++;
     storedItem.price = storedItem.item.price * storedItem.qty;
-    this.totalQty++;
-    this.totalPrice += storedItem.item.price;
+    this.totalItemQuantity++;
+    this.totalItemPrice += storedItem.item.price;
   }
-
+//List of items in cart
   this.generateArray = function () {
     var arr = [];
     for (var id in this.items) {
