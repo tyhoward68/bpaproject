dde05c8562f08f169585a917e23d354da65bbe0e
unknown <PS20VAdhikari@efcts.us>
2019-11-14 13:12:07 -0500

    cart

--- routes/index.js
+++ routes/index.js
@@ -65,4 +65,13 @@ router.get('/shopping-cart', function (req, res, next) {
   return res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
+router.get('/checkout', function(req, res, next){
+  if(!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/checkout', {total: cart.totalItemPrice});
+});
+
+
 module.exports = router;
