1138e47f9bf20b43e9a0312b632b1e06d3052336
unknown <PS20VAdhikari@efcts.us>
2019-12-05 11:43:18 -0500

    valdiation updates

--- routes/checkout.js
+++ routes/checkout.js
@@ -27,6 +27,11 @@ router.post('/', isLoggedIn, function(req, res, next) {
 
   var cart = new Cart(req.session.cart);
   req.checkBody('name', 'Invalid Name').isLength({ min: 5 })
+  req.checkBody('address', 'Invalid Address, Please inpuput your full address').isLength({ min: 20 })
+  req.checkBody('card-number', 'Invalid Card. Must be 16 digits long').isNumeric({ min: 16 })
+  req.checkBody('card-expiry-month', 'Invalid Month. If less then 10, place 0 before the number').isNumeric({ max: 12, min: 1 })
+  req.checkBody('card-expiry-Year', 'Invalid Year. Input whole year. Ex: 2040, 2031, 2020.').isNumeric({ max: 2025, min: 2019})
+  req.checkBody('card-cvc', 'Invalid CVC. Three digits on the back of your card.').isNumeric({ max: 3, min: 3 })
   var errors = req.validationErrors();
   if (errors) {
       var messages = [];
