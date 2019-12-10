6179bbb275dcbc147dd9c4cf5805628be84e2668
unknown <PS20GCarmo@efcts.us>
2019-12-10 11:49:17 -0500

    gabe

--- routes/checkout.js
+++ routes/checkout.js
@@ -26,12 +26,14 @@ router.post('/', isLoggedIn, function(req, res, next) {
   }
 
   var cart = new Cart(req.session.cart);
-  req.checkBody('name', 'Invalid Name').isLength({ min: 5 }).isAlpha()
-  req.checkBody('address', 'Invalid Address, Please inpuput your full address').isLength({ min: 20 })
-  req.checkBody('card-number', 'Invalid Card. Must be 16 digits long').isNumeric().isLength({min: 15})
-  req.checkBody('card-expiry-month', 'Invalid Month. If less then 10, place 0 before the number').isNumeric({ max: 12, min: 1 })
+  req.checkBody('name').not().isEmpty().isLength({ min: 5 }).withMessage('Name must have more than 5 characters');
+  req.checkBody('address').isLength({ min: 20 }).withMessage('Must be 20 characters long');
+  req.checkBody('card-number').isLength({min: 15}).withMessage('Invalid Card. Must be 16 digits long');
+  /*req.checkBody('card-expiry-month', 'Invalid Month. If less then 10, place 0 before the number').isNumeric({ max: 12, min: 1 })
   req.checkBody('card-expiry-Year', 'Invalid Year. Input whole year. Ex: 2040, 2031, 2020.').isNumeric({ max: 2025, min: 2019})
   req.checkBody('card-cvc', 'Invalid CVC. Three digits on the back of your card.').isNumeric()
+  */
+
   var errors = req.validationErrors();
   if (errors) {
       var messages = [];
